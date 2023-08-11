7677bab6ccca97319a71fe6f7ec94eb52d2d3353
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-10 21:14:17 -0300

    atividade criando tipos

---
+++ 17_ex_criando_tipos/public/dist/index.js
@@ -0,0 +1,26 @@
+const planets = [];
+function addPlanet(name, coordinates, situation) {
+    planets.push({
+        name,
+        coordinates,
+        situation,
+        satellites: []
+    });
+    alert(`O planeta ${name} foi adicionado com sucesso`);
+}
+function findPlanet(name) {
+    const planet = planets.find(planet => planet.name === name);
+    return planet;
+}
+function updateSituation(situation, planet) {
+    planet.situation = situation;
+    alert(`A situação do planeta ${planet.name} foi atualizada para ${situation}.`);
+}
+function addSatellites(name, planet) {
+    planet.satellites.push(name);
+    alert(`O satélite ${name} foi adicionado ao planeta ${planet.name}.`);
+}
+function removeSatellite(name, planet) {
+    planet.satellites = planet.satellites.filter(satellite => satellite !== name);
+    alert(`O satélite ${name} foi removido do planeta ${planet.name}.`);
+}
---
+++ 17_ex_criando_tipos/src/index.ts
@@ -0,0 +1,47 @@
+type PlanetSituation = "Habitado" | "Habitável" | "Inabitável" | "inexplorado"
+
+type PlanetCoordinates = [number, number, number, number]
+
+type Planet = {
+    name: string,
+    coordinates: PlanetCoordinates,
+    situation: PlanetSituation,
+    satellites: string[]
+}
+
+const planets: Planet[] = []
+
+function addPlanet(name: string, coordinates: PlanetCoordinates, situation: PlanetSituation){
+    planets.push(
+     {
+        name,
+        coordinates,
+        situation,
+        satellites: []
+    })
+
+    alert(`O planeta ${name} foi adicionado com sucesso`)
+}
+
+function findPlanet(name: string){
+    const planet = planets.find(planet => planet.name === name)
+    return planet
+}
+
+function updateSituation(situation: PlanetSituation, planet: Planet){
+    planet.situation = situation
+
+    alert(`A situação do planeta ${planet.name} foi atualizada para ${situation}.`)
+}
+
+function addSatellites(name:string, planet:Planet){
+    planet.satellites.push(name)
+
+    alert(`O satélite ${name} foi adicionado ao planeta ${planet.name}.`)
+}
+
+function removeSatellite(name:string, planet: Planet){
+    planet.satellites = planet.satellites.filter(satellite => satellite !== name)
+
+    alert(`O satélite ${name} foi removido do planeta ${planet.name}.`)
+}
